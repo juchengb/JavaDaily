71387daaee9697f891f2f929f3009b8a12712251
juchengb <fjchengou@gmail.com>
2024-02-05 17:35:35 +0800

    update

---
+++ src/day01/MultipleTable.java
@@ -0,0 +1,12 @@
+package day01;
+
+public class MultipleTable {
+	public static void main(String[] args){
+		for(int i=2; i<=9; i++){
+			for(int j=1; j<=9; j++){
+				System.out.print(i+"x"+j+"="+(i*j)+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
