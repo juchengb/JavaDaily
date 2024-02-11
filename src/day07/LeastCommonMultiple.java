725d2f630afe1da632b6e1cc9bc94296df157c60
juchengb <fjchengou@gmail.com>
2024-02-11 20:54:02 +0800

    20240211

--- src/day07/LeastCommonMultiple.java
+++ src/day07/LeastCommonMultiple.java
@@ -1,9 +1,9 @@
-package day03;
+package day07;
 
 import java.util.Scanner;
 
-// 輸入兩個數字並求出最大公因數
-public class GreatestCommonFactor {
+//輸入兩個數字並求出最小公倍數
+public class LeastCommonMultiple {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -12,13 +12,18 @@ public class GreatestCommonFactor {
 		System.out.print("請輸入第二個數字：");
 		int num2 = sc.nextInt();
 		int max = Math.max(num1, num2);
+		int greatestCommonFactor = 0;
 		
 		for (int i = max; i >= 1; i--) {
             if (num1 % i == 0 && num2 % i == 0) {
-                System.out.print("最大公因數為 " + i);
+            	greatestCommonFactor = i;
                 break;
             }
         }
+		
+		int ans = num1 * num2 / greatestCommonFactor;
+		
+		System.out.println("最小公倍數為 " + ans);
 
 	}
 
---
+++ src/day07/PredictHeight.java
@@ -0,0 +1,26 @@
+package day07;
+
+import java.util.Scanner;
+
+/*
+ * 預測子女身高
+ * （父親身高＋母親身高＋13）÷2＝男生成年後的身高（±5公分）
+ * （父親身高＋母親身高－13）÷2＝女生成年後的身高（±5公分）
+ * 
+ */
+public class PredictHeight {
+
+	public static void main(String[] args) {
+		System.out.print("請輸入父親身高(cm)：");
+        double fatherHeight = new Scanner(System.in).nextDouble();
+        System.out.print("請輸入母親身高(cm)：");
+        double motherHeight = new Scanner(System.in).nextDouble();
+        
+        double boy = (fatherHeight + motherHeight + 13 ) / 2;
+        double girl = (fatherHeight + motherHeight - 13 ) / 2;
+        
+        System.out.printf("兒子成年後的身高為 %.1f ~ %.1f /n女兒成年後的身高為 %.1f ~ %.1f", boy-5, boy+5, girl-5, girl+5);
+
+	}
+
+}
