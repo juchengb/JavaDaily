16fe3e65a7451dd95feccc3871d9a48e8b1b7bd7
juchengb <fjchengou@gmail.com>
2024-02-12 22:30:22 +0800

    20240212

---
+++ src/day08/ConcatenationOfArray.java
@@ -0,0 +1,26 @@
+package day08;
+
+import java.util.Arrays;
+
+public class ConcatenationOfArray {
+
+	public static void main(String[] args) {
+		int[] nums = {1, 2, 3, 4, 5};
+		getConcatenation(nums);
+
+	}
+	
+    public static int[] getConcatenation(int[] nums) {
+        int[] nums2 = Arrays.copyOf(nums, nums.length *2);
+        for (int i = 0; i < nums.length; i++) {
+        	nums2[i + nums.length] = nums[i];
+        }
+        
+        for (int n: nums2) {
+        	System.out.print(n + " ");
+        }
+        
+        return nums2;
+    }
+	
+}
