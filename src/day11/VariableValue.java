6bb35267657891998ef4ab72122249657c7c1bef
juchengb <fjchengou@gmail.com>
2024-02-15 23:55:14 +0800

    20240215

---
+++ src/day11/VariableValue.java
@@ -0,0 +1,20 @@
+package day11;
+
+// 2011. Final Value of Variable After Performing Operations
+public class VariableValue {
+	
+	public int finalValueAfterOperations(String[] operations) {
+	    int result = 0;
+
+	    for (String op : operations) {
+	    	if (op.charAt(1) == '+') {
+	    		result +=1;
+	    	} else if (op.charAt(1) == '-') {
+	    		result -=1;
+	    	}
+	    }
+	    	
+	    return result;
+	  }
+
+}
