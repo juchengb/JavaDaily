4885d9a27310a585129001d8d0b024224d1459fc
juchengb <fjchengou@gmail.com>
2024-02-17 23:44:33 +0800

    20240217

---
+++ src/day13/SmallestEvenMultiple.java
@@ -0,0 +1,11 @@
+package day13;
+
+// 2413. Smallest Even Multiple
+public class SmallestEvenMultiple {
+	public int smallestEvenMultiple(int n) {
+        if (n % 2 == 0) {
+            return n;
+        }
+        return n * 2;
+    }
+}
---
+++ src/day13/WhoMetTarget.java
@@ -0,0 +1,14 @@
+package day13;
+
+// 2798. Number of Employees Who Met the Target
+public class WhoMetTarget {
+	public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for (int i = 0; i < hours.length; i++) {
+            if (hours[i] >= target) {
+                count++;
+            }
+        }
+         return count;
+    }
+}
