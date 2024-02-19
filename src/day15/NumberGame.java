dc7f58c14f3e59644a1d52e058c2962d28257670
juchengb <fjchengou@gmail.com>
2024-02-19 22:51:52 +0800

    20240219

---
+++ src/day15/NumberGame.java
@@ -0,0 +1,17 @@
+package day15;
+
+import java.util.Arrays;
+
+// 2974. Minimum Number Game
+public class NumberGame {
+	public int[] numberGame(int[] nums) {
+        Arrays.sort(nums);
+        int arr[] = new int[nums.length];
+        for(int i = 0; i < nums.length; i += 2){
+            arr[i] = nums[i+1];
+            arr[i+1] = nums[i];
+        }
+
+        return arr;
+    }
+}
