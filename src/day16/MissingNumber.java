76a20ff5961a2dec1bdf007b8fc6e6adc6c74f1b
juchengb <fjchengou@gmail.com>
2024-02-20 13:29:42 +0800

    20240220

---
+++ src/day16/MissingNumber.java
@@ -0,0 +1,21 @@
+package day16;
+
+// 268. Missing Number
+public class MissingNumber {
+
+	public static void main(String[] args) {
+		int[] nums = {3,0,1};
+		missingNumber(nums);
+	}
+	
+	public static int missingNumber(int[] nums) {
+        int sum = 0;
+		int l = nums.length;
+		for (int i : nums) {
+			sum += i;
+			System.out.println(i);
+		}
+        return ((1 + l) * l / 2) - sum;
+    }
+
+}
