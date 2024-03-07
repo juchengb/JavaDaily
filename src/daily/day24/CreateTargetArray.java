47c0a72f849fec79a1921fa01c5fbc72bb2121ee
juchengb <fjchengou@gmail.com>
2024-03-08 00:06:07 +0800

    20240307 update

--- src/daily/day24/CreateTargetArray.java
+++ src/daily/day24/CreateTargetArray.java
@@ -3,21 +3,26 @@ package daily.day24;
 import java.util.ArrayList;
 import java.util.List;
 
+// 1389. Create Target Array in the Given Order
 public class CreateTargetArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {0, 1, 2, 3, 4};
+		int[] index = {0, 1, 2, 2, 1};
+		createTargetArray(nums, index);
 	}
 	
-	public int[] createTargetArray(int[] nums, int[] index) {
-        List<Integer> list=new ArrayList<>();
-        int[] result=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            list.add(index[i],nums[i]);
-        }     
-        for(int i=0;i<nums.length;i++){
-            result[i]=list.get(i);
+	public static int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        int[] result = new int[nums.length];
+        for(int i = 0; i < nums.length ; i++){
+            list.add(index[i], nums[i]);
+            System.out.println(list);
+        }
+        
+        for(int i = 0; i < nums.length ; i++){
+            result[i] = list.get(i);
+            System.out.println(result[i]);
         }
         return result;
     }
