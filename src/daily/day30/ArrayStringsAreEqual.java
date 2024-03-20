d1142324d96e9a22f7cd80c0aa83881ba9a3dfc9
juchengb <fjchengou@gmail.com>
2024-03-20 11:48:48 +0800

    20240320

---
+++ src/daily/day30/ArrayStringsAreEqual.java
@@ -0,0 +1,23 @@
+package daily.day30;
+
+public class ArrayStringsAreEqual {
+	
+	public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+		StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for (String i: word1) {
+            sb1.append(i);
+        }
+        
+        for(String j: word2) {
+        	sb2.append(j);
+        }
+        
+        if (sb1.toString().equals(sb2.toString())) {
+        	return true;
+        }
+        return false;
+    }
+
+}
