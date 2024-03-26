c18e68c05de4cdc0d5c112055357d64c139e736e
juchengb <fjchengou@gmail.com>
2024-03-26 22:06:11 +0800

    20240326

---
+++ src/daily/day35/Game1A2B.java
@@ -0,0 +1,57 @@
+package daily.day35;
+
+import java.util.Scanner;
+
+public class Game1A2B {
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		System.out.println("歡迎來到數字遊戲");
+		String ans;
+		String result;
+		String pc = generateRandom();
+		
+		do {
+			ans = input();
+			result = judge(ans, pc);
+			System.out.println(ans + " " + result);
+		} while (!result.equals("4A0B"));
+		System.out.print("恭喜過關！");
+	}
+	
+	public static String generateRandom() {
+	    StringBuilder pc = new StringBuilder();
+	    boolean[] used = new boolean[10];
+
+	    while (pc.length() < 4) {
+	        int digit = (int) (Math.random() * 10);
+	        if (!used[digit]) {
+	            pc.append(digit);
+	            used[digit] = true;
+	        }
+	    }
+
+	    return pc.toString();
+	}
+	
+	public static String input() {
+		System.out.print("請輸入 4 個 0 ~ 9 數字(數字不得重複)：");
+		 return sc.next();
+	} 
+	
+	public static String judge(String ans, String pc) {
+		int a = 0;
+		int b = 0;
+		for (int i = 0; i < 4; i++) {
+			if (pc.charAt(i) == ans.charAt(i)) {
+				a++;
+			}
+			for (int j = 0; j < 4; j++) {
+				if (pc.charAt(i) == ans.charAt(j) && i != j) {
+					b++;
+				}
+			}
+		}
+		return a + "A" + b + "B";
+	}
+}
