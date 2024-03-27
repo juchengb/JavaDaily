4cb47db6a6e56190daa6345a15c43ac8444a14d9
juchengb <fjchengou@gmail.com>
2024-03-27 14:06:22 +0800

    update Game1A2B

--- src/daily/day35/Game1A2B.java
+++ src/daily/day35/Game1A2B.java
@@ -1,6 +1,8 @@
 package daily.day35;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Game1A2B {
 	static Scanner sc = new Scanner(System.in);
@@ -13,6 +15,11 @@ public class Game1A2B {
 		
 		do {
 			ans = input();
+			
+			while (!isValidInput(ans)) {
+				System.out.println("輸入格式錯誤，請重新輸入四個不重複的數字");
+                ans = input();
+			}
 			result = judge(ans, pc);
 			System.out.println(ans + " " + result);
 		} while (!result.equals("4A0B"));
@@ -36,8 +43,22 @@ public class Game1A2B {
 	
 	public static String input() {
 		System.out.print("請輸入 4 個 0 ~ 9 數字(數字不得重複)：");
-		 return sc.next();
+		return sc.next();
 	} 
+	
+	public static boolean isValidInput(String input) {
+        if (input.length() != 4)
+            return false;
+
+        Set<Character> set = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            if (c < '0' || c > '9')
+                return false;
+            set.add(c);
+        }
+        return set.size() == 4;
+    }
+	
 	
 	public static String judge(String ans, String pc) {
 		int a = 0;
