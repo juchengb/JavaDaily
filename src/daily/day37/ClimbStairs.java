19bc824efebf1a9e18f79530042bff2367c29de5
juchengb <fjchengou@gmail.com>
2024-03-28 13:53:35 +0800

    20240328

---
+++ src/daily/day37/ClimbStairs.java
@@ -0,0 +1,16 @@
+package daily.day37;
+
+
+// 70. Climbing Stairs
+public class ClimbStairs {
+	public int climbStairs(int n) {
+        int[] num = new int[n + 1];
+        num[0] = 1;
+        num[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            num[i] = num[i - 1] + num[i - 2];
+
+        return num[n];
+    }
+}
