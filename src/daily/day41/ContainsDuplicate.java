f1468c1ce5fa4a7737f5ed5de45f3bbe3f37c153
juchengb <fjchengou@gmail.com>
2024-04-06 17:06:16 +0800

    20240406

---
+++ src/daily/day41/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package daily.day41;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Arrays & Hashing
+// 217. Contains Duplicate
+public class ContainsDuplicate {
+	
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> numsSet = new HashSet<>();
+		for (int i: nums) {
+			if (numsSet.contains(i)) {
+				return true;
+			}
+			numsSet.add(i);
+		}
+		return false;
+    }
+}
