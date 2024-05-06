b307afb849e5b8165cb0d3ad083a5e5735567f1a
Yvonne <fjchengou@gmail.com>
2024-05-06 22:49:36 +0800

    up

---
+++ src/daily/day47/Test.java
@@ -0,0 +1,10 @@
+package daily.day47;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/daily/day48/ScoreOfString.java
@@ -0,0 +1,10 @@
+package daily.day48;
+
+public class ScoreOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
