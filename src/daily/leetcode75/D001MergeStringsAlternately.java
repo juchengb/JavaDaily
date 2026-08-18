d290dd01a817a4c86d203cbb336d479aeb795d92
Yvonne Cheng <151800584+juchengb@users.noreply.github.com>
2026-08-18 02:58:29 +0000

    D001

--- src/daily/leetcode75/D001MergeStringsAlternately.java
+++ src/daily/leetcode75/D001MergeStringsAlternately.java
@@ -2,6 +2,19 @@
 public class D001MergeStringsAlternately {
 
     public String mergeAlternately(String word1, String word2) {
-        
+        int w1l = word1.length();
+        int w2l = word2.length();
+        int i = 0, j = 0;
+        StringBuilder ans = new StringBuilder();
+        while(i < w1l || j < w2l){
+            if(i < w1l){
+                ans.append(word1.charAt(i++));
+            }
+            if(j < w2l){
+                ans.append(word2.charAt(j++));
+            }           
+        }
+        return ans.toString();
     }
+
 }
