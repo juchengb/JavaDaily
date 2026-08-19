fecbcaeb6567675de19f7dd541cdb2410feb2b33
Yvonne Cheng <151800584+juchengb@users.noreply.github.com>
2026-08-19 06:00:16 +0000

    D002

---
+++ src/daily/leetcode75/D002GcdOfStrings.java
@@ -0,0 +1,24 @@
+// 1071. Greatest Common Divisor of Strings
+public class D002GcdOfStrings {
+
+    public String gcdOfStrings(String str1, String str2) {
+        // Check if they have non-zero GCD string.
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+
+    // 歐幾里得演算法（Euclidean Algorithm）
+    public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }    
+    }
+
+}
