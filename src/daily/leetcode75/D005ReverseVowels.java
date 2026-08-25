7c2795aad631e18b35761553541bf393b174c48f
Yvonne Cheng <151800584+juchengb@users.noreply.github.com>
2026-08-25 03:41:52 +0000

    D005

---
+++ src/daily/leetcode75/D005ReverseVowels.java
@@ -0,0 +1,33 @@
+// 345. Reverse Vowels of a String
+public class D005ReverseVowels {
+
+    // 雙指標 Two Pointers
+    public String reverseVowels(String s) {
+
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            
+            while (left < right && !isVowel(chars[left])) { left++; } // 左指標向右尋找母音
+            while (left < right && !isVowel(chars[right])) { right--; }// 右指標向左尋找母音
+                
+            // 交換
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(chars);
+
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+}
