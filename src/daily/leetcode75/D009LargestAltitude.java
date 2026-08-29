b513b5925555c7ab35fd61da48aa5f5e8ae0b4f3
Yvonne Cheng <151800584+juchengb@users.noreply.github.com>
2026-08-29 09:32:52 +0000

    D009

---
+++ src/daily/leetcode75/D009LargestAltitude.java
@@ -0,0 +1,16 @@
+package daily.leetcode75;
+// 1732. Find the Highest Altitude
+public class D009LargestAltitude {
+
+    public int largestAltitude(int[] gain) {
+
+        int current = 0, max = 0;
+
+        for (int i : gain) {
+            current += i;
+            max = Math.max(current, max);
+        }
+        return max;
+    }
+
+}
